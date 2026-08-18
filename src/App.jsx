6d13894f9012a3bd489a39c27bb32ac7af115768
mjkim <promunday@gmail.com>
2026-08-18 13:54:26 +0900

    Custom Hook 사용해서 useTodo로 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,53 @@
-import { useEffect, useState } from 'react';
-import TodoInput from './TodoInput';
+import { useState, useRef } from 'react';
+import useTodos from "./useTodos"
 import TodoItem from './TodoItem';
 
 function App(){
 
 	const [todo, setTodo] = useState("");
-	const [todos, setTodos] = useState([]);
-	const [isLoaded, setIsLoaded] = useState(false);
 
-	// 로컬스토리지 불러오기
-	useEffect(() => {
-		const savedTodos = localStorage.getItem("todos");
-		if(savedTodos){
-			setTodos(JSON.parse(savedTodos));
-		}
+	const {
+		todos,
+		addTodo,
+		deleteTodo,
+		completeTodo,
+		modifyTodo
+	} = useTodos();
 
-		setIsLoaded(true);
-	}, []);
-
-	// 로컬스토리지 저장
-	useEffect(() => {
-		if(!isLoaded) return;
-		localStorage.setItem("todos", JSON.stringify(todos));
-	}, [todos, isLoaded]);
-
-	// 할일추가
 	const handleAddTodo = () => {
-		setTodos([
-			...todos, {
-				id: Date.now(),
-				text: todo,
-				completed: false
-			}]);
+		if(!todo.trim()) return;
+
+        inputRef.current.focus();
+		addTodo(todo);
 		setTodo("");
 	}
 
-	// 할일삭제
-	const handleDeleteTodo = (id) => {
-		setTodos(
-			todos.filter((item) => item.id !== id)
-		);
-	}
-
-	// 할일완료
-	const handleCompleteTodo = (id) => {
-		setTodos(
-			todos.map((item) => {
-				if(item.id === id){
-					return {
-						...item,
-						completed: !item.completed
-					}
-				}
-				return item;
-			})
-		)
-	}
-
-	// 할일수정
-	const handleModifyTodo = (id, text) => {
-		setTodos(
-			todos.map((item) => {
-				if(item.id === id){
-					return {
-						...item,
-						text: text
-					}
-				}
-				return item
-			})
-		)
-	}
-
+    const inputRef = useRef(null);
 
 	return (
 		<>
 			<h1>REACT TODO APP</h1>
-			<TodoInput
-				todo={todo}
-				onChange={setTodo}
-				onAdd={handleAddTodo}
+			<input
+				type="text"
+				placeholder="할 일을 입력하세요."
+                ref={inputRef}
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
+				onKeyDown={(e) => {
+					if(e.key === "Enter"){
+						handleAddTodo();
+					}
+				}}
 			/>
+			<button onClick={handleAddTodo}>추가</button>
 			<ul className="todo-list">
 				{todos.map((todo) => (
 					<TodoItem
 						key={todo.id}
 						todo={todo}
-						onDelete={handleDeleteTodo}
-						onComplete={handleCompleteTodo}
-						onModify={handleModifyTodo}
+						onDelete={deleteTodo}
+						onComplete={completeTodo}
+						onModify={modifyTodo}
 					/>
 				))}
 			</ul>
---
+++ src/useTodos.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+
+function useTodos(){
+	
+	const [todos, setTodos] = useState([]);
+
+	// 처음 LocalStorage에서 불러오기
+	useEffect(() => {
+		const savedTodos = localStorage.getItem("todos");
+
+		if(savedTodos) {
+			setTodos(JSON.parse(savedTodos));
+		}
+	}, []);
+
+	// todos가 변경되면 저장
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
+	// todo 추가
+	const addTodo = (text) => {
+		setTodos(prev => [
+			...prev,
+			{
+				id: Date.now(),
+				text,
+				completed: false
+			}
+		]);
+	};
+
+	// todo 삭제
+	const deleteTodo = (id) => {
+		setTodos(prev =>
+			prev.filter(item => item.id !== id)
+		);
+	};
+
+	// todo 완료 체크
+	const completeTodo = (id) => {
+		setTodos(prev =>
+			prev.map(item => {
+				if(item.id === id){
+					return {
+						...item,
+						completed: !item.completed
+					}
+				}
+				return item;
+			})
+		);
+	}
+
+	// todo 내용 수정
+	const modifyTodo = (id, text) => {
+		setTodos(prev =>
+			prev.map(item => {
+				if(item.id === id){
+					return {
+						...item,
+						text
+					};
+				}
+				return item;
+			})
+		)
+	}
+
+	return {
+		todos,
+		addTodo,
+		deleteTodo,
+		completeTodo,
+		modifyTodo
+	}
+
+
+}
+
+export default useTodos;
